dba708d78471cb006369d0a13e512aaeb4ec368f
Mykhailo Marchuk <mishaakamichael999@gmail.com>
2024-08-22 08:00:06 +0300

    Initial commit

---
+++ src/main/java/com/uni/universityconsoleapp/UniversityConsoleAppApplication.java
@@ -0,0 +1,13 @@
+package com.uni.universityconsoleapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversityConsoleAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniversityConsoleAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uni/universityconsoleapp/UniversityConsoleAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uni.universityconsoleapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniversityConsoleAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
